d2e77752f3ffcfb33f3f3fd0f3fce8e1f4bc5c9d
JFinchley <46974334+JFinchley@users.noreply.github.com>
2019-10-19 20:50:38 +0100

    Update Croquis-Collector-v2.py
    changed for new website design

--- Croquis-Collector-v2.py
+++ Croquis-Collector-v2.py
@@ -1,4 +1,4 @@
-# version 2.0 for new website design
+# version 2.3 for new website design
 
 import json
 import os
@@ -16,87 +16,66 @@ def download(url, file_name):
 
 
 print("Croquis-Collector-v2 started.")
-
 # directories list contains URLs of model indices
-indicesList = ["/model-index-1"]  # starting page
+indicesList = ["/model-index-directory"]  # starting page
 jsURLs = []  # contains script.js urls scrapped from source html
 jsonURLs = []   # contains relevant json urls that were within jsURLs
+baseDir = ""
 
 # create list of archive URLs by looping through all indices
 for listItem in indicesList:
-    print("Downloading http://www.onairvideo.com" + str(listItem))
-    download("http://www.onairvideo.com" + str(listItem), "tempfile.html")  # saves file for scraping
-    tempFileHTML = open("tempfile.html")
-    soup = bs4.BeautifulSoup(tempFileHTML, "html.parser")
-
-    # make a list to save all links by scraping tempfile.html
-    href = []
+    print("\rInterrogating http://www.onairvideo.com" + str(listItem), end="", flush=True)
+    tempFileHTML = requests.get("http://www.onairvideo.com" + str(listItem))  # saves file for scraping
+    soup = bs4.BeautifulSoup(tempFileHTML.content, "html.parser")
+    href = []   # make a list to save all links by scraping soup
     for link in soup.find_all("a"):
         href.append(link.get("href"))
-
-    # record the links which are indexes and contain links to models
-    for item in href:
-        if item is not None:
-            if "model-index-pg" in item:
-                if item not in indicesList:
-                    if "#" not in item:  # loops through temporary list and adds to directoireslist if unique
-                        indicesList.append(item)
-
-    # make a list of each model link by json data from source html
-    dataJS = str(soup)  # using re to scrap JSON data (held in script.js files)
+    href = list(set(href))  # remove duplicates
+    for item in href:   # record the unique links which are indexes and contain links to models
+        if item is not None and "model-index" in item:
+            if item.find("#", 0) != -1:
+                item = item[0:item.find("#",0)]
+            if item not in indicesList:
+                indicesList.append(item)
+    dataJS = str(soup)  # using re to scrap JSON data (held in script.js files) to find links to models
     pattern = r"//img1.wsimg.com/blobby/go/e447419f-7200-4240-9090-26bc61d6e2b2/gpub/\S+?/script.js"
-    jsURLs = re.findall(pattern, dataJS)     # jsURLs used as temporary storage
-    jsURLs = list(set(jsURLs))    # remove duplicates
+    jsURLs = list(set(re.findall(pattern, dataJS)))     # jsURLs used for jsonURLs on this page
     for item in jsURLs:
-        if item is not None:
-            if item not in jsonURLs:
-                jsonURLs.append(item)
-
-    tempFileHTML.close()  # file clean up
-
-print("Downloading and interpreting " + str(len(jsonURLs)) + " JSON files to find model and photo information.")
+        if item is not None and item not in jsonURLs:
+            jsonURLs.append(item)   # jsonURLs holds json urls across all indexes
+print("\rInterrogated all site directories.")   # else the json print will overwrite the downloads
 
 # cycle through the found script.js s and get the gallery JSON files to analyse
-modelList = []  # format [[modelName1, [modelURL1, modelURLn]], [modelNamen, [modelURL1, modelURLn]]]
+photoURLs = []  # for all
 for listItem in jsonURLs:
-    download("http:" + str(listItem), "tempfile.json")
-    with open("tempfile.json") as tempFileJSON:
-        data = tempFileJSON.read()
-        if "Gallery4" in data:  # all the gallery JSON files have Gallery4 in them
-            data = data[data.find("})({") + 3:data.rfind("}") + 1]  # strips javascript from JSON
-            data = data.replace("\u002F", "/")  # cleans JSON by removing backslash characters
-            data = data.replace("/:/", "")  # removes the useless /:/ from the (photo) urls
-            dataStore = json.loads(data)
-            modelName = dataStore["title"]
-            photoURLs = []
-            for item in dataStore["paginatedImages"]:
-                for element in item:  # the JSON stores urls in a ["paginatedImages"][i]["url"] format
-                    photoURLs.append(element["url"])  # with i being an integer
-            photoURLs = list(set(photoURLs))    # remove duplicates
-            modelList.append([modelName, photoURLs])    # add to modelList
-        tempFileJSON.close()
+    print("\rInterrogating file " + str(jsonURLs.index(listItem) + 1) + " of " + str(len(jsonURLs)) +
+          " JSON files to find model and photo information.", flush=True, end="")
+    data = str(requests.get("http:" + str(listItem)).content)
+    if "galleryImages" in data:  # all the gallery JSON files have galleryImages in them
+        data = data[data.find("})({") + 3:data.rfind("}") + 1]  # strips javascript from JSON
+        data = data.replace(r"\\u002F", "/")  # cleans JSON by removing backslash characters
+        data = data.replace("\\", "")   # there are the occasional random backslash in names
+        dataStore = json.loads(data)
+        for item in dataStore["galleryImages"][0]:  # json format ["galleryImages"][0][]["image"]["image"]
+            photoURLs.append(item["image"]["image"].rsplit("/", 1)[1])
+photoURLs = list(set(photoURLs))    # remove duplicates
+photoURLs.sort()
 
-print("Found " + str(len(modelList)) + " models and " +
-      str(str(modelList).count(",") + 1 - len(modelList)) + " photos.")
+print("\n" + str(len(photoURLs)) + " photos found, ", end="")
+totalCount = len(photoURLs)
 
-# cycle through model name downloading photos for folders that do not already exist
-for listItem in modelList:
-    modelName = listItem[0]
-    photoURLs = listItem[1]
-    if os.path.isdir(modelName):
-        if len(os.listdir(modelName)) >= len(photoURLs):
-            print("Skipping model " + str(modelName) + ". Existing images in folder.")
-            continue  # skip this listItem, model folder exists with correct number of images
-    else:
-        os.mkdir(modelName)
-    for item in photoURLs:
-        print("Downloading model " + str(modelName) + ", image " + str(photoURLs.index(item) + 1) +
-              " of " + str(len(photoURLs)))
-        download("http:" + str(item),
-                 str(os.getcwd()) + "\\" + str(modelName) + "\\" + str(photoURLs.index(item) + 1) + ".jpg")
+# cycle through and remove images that already exist
+for listItem in photoURLs[:]:
+    if os.path.isfile(baseDir + listItem):
+        photoURLs.remove(listItem)
 
-# Clean up tempfiles
-os.remove("tempfile.html")
-os.remove("tempfile.json")
+print("skipped " + str(totalCount - len(photoURLs)) + " existing images in folder.")
 
-print("Croquis-Collector-v2 complete.")
+# download images that have not been downloaded previously
+for listItem in photoURLs:
+    print("\rDownloading image " + str(photoURLs.index(listItem) + 1) + " of " + str(len(photoURLs)) + ": " + listItem,
+          end="", flush=True)
+    download("https://img1.wsimg.com/isteam/ip/e447419f-7200-4240-9090-26bc61d6e2b2/" + str(listItem),
+             baseDir + listItem)
+
+print("\nCroquis-Collector-v2 complete.")
